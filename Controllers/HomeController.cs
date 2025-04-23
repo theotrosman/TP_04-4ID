09d49ca56aff566045bc0607b1f895776262183b
THEO VALENTÍN TROSMAN <49092919@ort>
2025-04-23 08:09:30 -0300

    Creación de Biblioteca

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -15,6 +15,8 @@ public class HomeController : Controller
 
     public IActionResult Index()
     {
+        Dictionary<int, Disco> discos = Biblioteca.InicializarGrupo();
+        ViewBag.integrantes = discos;
         return View();
     }
 }
---
+++ Models/Biblioteca.cs
@@ -0,0 +1,10 @@
+class Grupo
+{
+    public static Dictionary<int, Disco> discos { get; private set; } = InicializarBiblioteca();
+
+
+public static Dictionary<int, Disco> InicializarBiblioteca()
+    {
+        
+    }
+}
